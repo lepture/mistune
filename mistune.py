b1b97861ff875f40c408d3229b840dff92a803ce
Hsiaoming Yang <me@lepture.com>
2014-02-25 17:54:05 +0800

    With docstrings

--- mistune.py
+++ mistune.py
@@ -15,7 +15,8 @@ __author__ = 'Hsiaoming Yang <me@lepture.com>'
 __all__ = [
     'BlockGrammar', 'BlockLexer',
     'InlineGrammar', 'InlineLexer',
-    'Markdown', 'markdown',
+    'Renderer', 'Markdown',
+    'markdown', 'escape',
 ]
 
 
@@ -36,11 +37,19 @@ def _keyify(key):
 _escape_pattern = re.compile(r'&(?!#?\w+;)')
 
 
-def escape(text, quote=None, force_amp=False):
-    if force_amp:
-        text = text.replace('&', '&amp;')
-    else:
+def escape(text, quote=False, smart_amp=True):
+    """Replace special characters "&", "<" and ">" to HTML-safe sequences.
+
+    The original cgi.escape will always escape "&", but you can control
+    this one for a smart escape amp.
+
+    :param quote: if set to True, " and ' will be escaped.
+    :param smart_amp: if set to False, & will always be escaped.
+    """
+    if smart_amp:
         text = _escape_pattern.sub('&amp;', text)
+    else:
+        text = text.replace('&', '&amp;')
     text = text.replace('<', '&lt;')
     text = text.replace('>', '&gt;')
     if quote:
@@ -59,6 +68,8 @@ def preprocessing(text, tab=4):
 
 
 class BlockGrammar(object):
+    """Grammars for block level tokens."""
+
     _tag = (
         r'(?!(?:'
         r'a|em|strong|small|s|cite|q|dfn|abbr|data|time|code|'
@@ -142,6 +153,8 @@ class BlockGrammar(object):
 
 
 class BlockLexer(object):
+    """Block level lexer for block grammars."""
+
     def __init__(self, rules=None, **kwargs):
         self.options = kwargs
 
@@ -405,6 +418,8 @@ class BlockLexer(object):
 
 
 class InlineGrammar(object):
+    """Grammars for inline level tokens."""
+
     escape = re.compile(r'^\\([\\`*{}\[\]()#+\-.!_>~|])')
     tag = re.compile(
         r'^<!--[\s\S]*?-->|'
@@ -444,6 +459,8 @@ class InlineGrammar(object):
 
 
 class InlineLexer(object):
+    """Inline level lexer for inline grammars."""
+
     def __init__(self, renderer, rules=None, **kwargs):
         self.options = kwargs
 
@@ -590,7 +607,7 @@ class InlineLexer(object):
         return self.renderer.emphasis(text)
 
     def output_code(self, m):
-        text = escape(m.group(2), force_amp=True)
+        text = escape(m.group(2), smart_amp=False)
         return self.renderer.codespan(text)
 
     def output_linebreak(self, m):
@@ -605,88 +622,163 @@ class InlineLexer(object):
 
 
 class Renderer(object):
+    """The default HTML renderer for rendering Markdown.
+    """
+
     def __init__(self, **kwargs):
         self.options = kwargs
 
     def block_code(self, code, lang=None):
+        """Rendering block level code. pre > code.
+
+        :param code: text content of the code block.
+        :param lang: language of the given code.
+        """
         if not lang:
-            code = escape(code, force_amp=True)
+            code = escape(code, smart_amp=False)
             return '<pre><code>%s\n</code></pre>\n' % code
-        code = escape(code, quote=True, force_amp=True)
+        code = escape(code, quote=True, smart_amp=False)
         return '<pre><code class="lang-%s">%s\n</code>\n</pre>' % (lang, code)
 
     def block_quote(self, text):
+        """Rendering <blockquote> with the given text.
+
+        :param text: text content of the blockquote.
+        """
         return '<blockquote>%s\n</blockquote>' % text
 
     def block_html(self, html):
+        """Rendering block level pure html content.
+
+        :param html: text content of the html snippet.
+        """
         if self.options.get('skip_style'):
             if html.lower().startswith('<style'):
                 return ''
         return html
 
     def header(self, text, level, raw=None):
+        """Rendering header/heading tags like <h1> <h2>.
+
+        :param text: rendered text content for the header.
+        :param level: a number for the header level, for example: 1.
+        :param raw: raw text content of the header.
+        """
         return '<h%d>%s</h%d>\n' % (level, text, level)
 
     def hrule(self):
+        """Rendering method for <hr> tag."""
         if self.options.get('use_xhtml'):
             return '<hr />\n'
         return '<hr>\n'
 
     def list(self, body, ordered=True):
+        """Rendering list tags like <ul> and <ol>.
+
+        :param body: body contents of the list.
+        :param ordered: whether this list is ordered or not.
+        """
         tag = 'ul'
         if ordered:
             tag = 'ol'
         return '<%s>\n%s</%s>\n' % (tag, body, tag)
 
     def list_item(self, text):
+        """Rendering list item snippet. Like <li>."""
         return '<li>%s</li>\n' % text
 
     def paragraph(self, text):
+        """Rendering paragraph tags. Like <p>."""
         return '<p>%s</p>\n' % text
 
     def table(self, header, body):
+        """Rendering table element. Wrap header and body in it.
+
+        :param header: header part of the table.
+        :param body: body part of the table.
+        """
         return (
             '<table>\n<thead>%s</thead>\n'
             '<tbody>\n%s</tbody>\n</table>\n'
         ) % (header, body)
 
-    def table_row(self, text):
-        return '<tr>\n%s</tr>\n' % text
+    def table_row(self, content):
+        """Rendering a table row. Like <tr>.
 
-    def table_cell(self, text, **flags):
+        :param content: content of current table row.
+        """
+        return '<tr>\n%s</tr>\n' % content
+
+    def table_cell(self, content, **flags):
+        """Rendering a table cell. Like <th> <td>.
+
+        :param content: content of current table cell.
+        :param header: whether this is header or not.
+        :param align: align of current table cell.
+        """
         if flags['header']:
             tag = 'th'
         else:
             tag = 'td'
         align = flags['align']
         if not align:
-            return '<%s>%s</%s>\n' % (tag, text, tag)
-        return '<%s style="text-align:%s">%s</%s>\n' % (tag, align, text, tag)
+            return '<%s>%s</%s>\n' % (tag, content, tag)
+        return '<%s style="text-align:%s">%s</%s>\n' % (
+            tag, align, content, tag
+        )
 
     def double_emphasis(self, text):
+        """Rendering **strong** text.
+
+        :param text: text content for emphasis.
+        """
         return '<strong>%s</strong>' % text
 
     def emphasis(self, text):
+        """Rendering *emphasis* text.
+
+        :param text: text content for emphasis.
+        """
         return '<em>%s</em>' % text
 
     def codespan(self, text):
+        """Rendering inline `code` text.
+
+        :param text: text content for inline code.
+        """
         return '<code>%s</code>' % text
 
     def linebreak(self):
+        """Rendering line break like <br>."""
         if self.options.get('use_xhtml'):
             return '<br />'
         return '<br>'
 
     def strikethrough(self, text):
+        """Rendering ~~strikethrough~~ text.
+
+        :param text: text content for strikethrough.
+        """
         return '<del>%s</del>' % text
 
     def autolink(self, link, is_email=False):
+        """Rendering a given link or email address.
+
+        :param link: link content or email address.
+        :param is_email: whether this is an email or not.
+        """
         text = link
         if is_email:
             link = 'mailto:%s' % link
         return '<a href="%s">%s</a>' % (link, text)
 
     def link(self, link, title, text):
+        """Rendering a given link with content and title.
+
+        :param link: href link for <a> tag.
+        :param title: title content for `title` attribute.
+        :param text: text content for description.
+        """
         if 'javascript:' in link:
             # for safety
             return ''
@@ -694,21 +786,36 @@ class Renderer(object):
             return '<a href="%s">%s</a>' % (link, text)
         return '<a href="%s" title="%s">%s</a>' % (link, title, text)
 
-    def image(self, link, title, text):
+    def image(self, src, title, text):
+        """Rendering a image with title and text.
+
+        :param src: source link of the image.
+        :param title: title text of the image.
+        :param text: alt text of the image.
+        """
         if not title:
-            html = '<img src="%s" alt="%s"' % (link, text)
+            html = '<img src="%s" alt="%s"' % (src, text)
         else:
-            html = '<img src="%s" alt="%s" title="%s"' % (link, text, title)
+            html = '<img src="%s" alt="%s" title="%s"' % (src, text, title)
         if self.options.get('use_xhtml'):
             return '%s />' % html
         return '%s>' % html
 
     def raw_html(self, html):
+        """Rendering span level html snippet.
+
+        :param html: html snippet.
+        """
         if self.options.get('skip_html'):
             return escape(html)
         return html
 
     def footnote_ref(self, key, index):
+        """Rendering the ref anchor of a footnote.
+
+        :param key: identity key for the footnote.
+        :param index: the index count of current footnote.
+        """
         html = (
             '<sup class="footnote-ref" id="fnref-%s">'
             '<a href="#fn-%s" rel="footnote">%d</a></sup>'
@@ -716,6 +823,11 @@ class Renderer(object):
         return html
 
     def footnote_item(self, key, text):
+        """Rendering a footnote item.
+
+        :param key: identity key for the footnote.
+        :param text: text content of the footnote.
+        """
         back = (
             '<a href="#fnref-%s" rev="footnote">&#8617;</a>'
         ) % escape(key)
@@ -728,6 +840,10 @@ class Renderer(object):
         return html
 
     def footnotes(self, text):
+        """Wrapper for all footnotes.
+
+        :param text: contents of all footnotes.
+        """
         return '<div class="footnotes">\n<ol>%s</ol>\n</div>\n' % text
 
 

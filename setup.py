e031734c2a5130f1778244bdd36a26d1cee2286c
Hsiaoming Yang <me@lepture.com>
2014-03-29 22:29:16 +0800

    More classifiers

--- setup.py
+++ setup.py
@@ -47,6 +47,12 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Text Processing :: Markup',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]

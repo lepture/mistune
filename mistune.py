a4a4d705a6ca972e4af5667fb9ef3ab31b96e0a0
Matthias Endler <matthias-endler@gmx.net>
2015-10-22 17:20:36 +0200

    Change wording in description for hard_wrap parameter

--- mistune.py
+++ mistune.py
@@ -1134,7 +1134,7 @@ def markdown(text, escape=True, **kwargs):
     :param text: markdown formatted text content.
     :param escape: if set to False, all html tags will not be escaped.
     :param use_xhtml: output with xhtml tags.
-    :param hard_wrap: if set to True, it will has GFM line breaks feature.
+    :param hard_wrap: if set to True, it will use the GFM line breaks feature.
     :param parse_block_html: parse text only in block level html.
     :param parse_inline_html: parse text only in inline level html.
     """

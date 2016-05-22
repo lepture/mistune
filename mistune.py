2a33458a589659d2743961d762aff8b8eb7e1890
Hsiaoming Yang <me@lepture.com>
2016-05-23 00:38:06 +0900

    Fix html attribute regex. Close #99

--- mistune.py
+++ mistune.py
@@ -35,7 +35,7 @@ _inline_tags = [
 ]
 _pre_tags = ['pre', 'script', 'style']
 _valid_end = r'(?!:/|[^\w\s@]*@)\b'
-_valid_attr = r'''"[^"]*"|'[^']*'|[^'">]'''
+_valid_attr = r'''\s*[a-zA-Z\-](?:\=(?:"[^"]*"|'[^']*'))*'''
 _block_tag = r'(?!(?:%s)\b)\w+%s' % ('|'.join(_inline_tags), _valid_end)
 _scheme_blacklist = ('javascript', 'data', 'vbscript')
 

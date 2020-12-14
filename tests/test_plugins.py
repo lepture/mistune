cea561eb0641cff6ce1604f127c75485c0bdd992
Giacomo Caironi <giacomo.caironi@gmail.com>
2020-12-14 17:14:32 +0100

    Add definition list plugin

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -3,29 +3,27 @@ from tests import BaseTestCase, fixtures
 
 
 def load_plugin(plugin_name, ast=False):
-    _plugin = getattr(plugins, 'plugin_{}'.format(plugin_name))
+    _plugin = getattr(plugins, "plugin_{}".format(plugin_name))
 
     class TestPlugin(BaseTestCase):
-        md = Markdown(
-            renderer=HTMLRenderer(escape=False),
-            plugins=[_plugin]
-        )
+        md = Markdown(renderer=HTMLRenderer(escape=False), plugins=[_plugin])
 
     def test_ast_renderer(self):
         md = Markdown(renderer=AstRenderer(), plugins=[_plugin])
-        data = fixtures.load_json(plugin_name + '.json')
-        self.assertEqual(md(data['text']), data['tokens'])
+        data = fixtures.load_json(plugin_name + ".json")
+        self.assertEqual(md(data["text"]), data["tokens"])
 
     if ast:
-        test_ast_renderer.__doc__ = 'Run {} ast renderer'.format(plugin_name)
-        setattr(TestPlugin, 'test_ast_renderer', test_ast_renderer)
+        test_ast_renderer.__doc__ = "Run {} ast renderer".format(plugin_name)
+        setattr(TestPlugin, "test_ast_renderer", test_ast_renderer)
 
-    TestPlugin.load_fixtures(plugin_name + '.txt')
-    globals()['TestPlugin_' + plugin_name] = TestPlugin
+    TestPlugin.load_fixtures(plugin_name + ".txt")
+    globals()["TestPlugin_" + plugin_name] = TestPlugin
 
 
-load_plugin('url')
-load_plugin('strikethrough')
-load_plugin('footnotes', True)
-load_plugin('table', True)
-load_plugin('task_lists', True)
+load_plugin("url")
+load_plugin("strikethrough")
+load_plugin("footnotes", True)
+load_plugin("table", True)
+load_plugin("task_lists", True)
+load_plugin("def_list")

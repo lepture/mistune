a92d1fd0aaafd050f29385300b761162cf393016
Giacomo Caironi <giacomo.caironi@gmail.com>
2021-08-02 13:01:56 +0200

    Make admonition directive html more similar to python-markdown

--- tests/test_directive.py
+++ tests/test_directive.py
@@ -17,7 +17,7 @@ class TestPluginAdmonition(TestCase):
         s = '.. note:: Warnning\n\n   message'
         html = md(s)
         self.assertIn('class="admonition note"', html)
-        self.assertIn('<h1>Warnning</h1>', html)
+        self.assertIn('<p class="admonition-title">Warnning</p>', html)
         self.assertIn('<p>message</p>', html)
 
     def test_code_admonition(self):
@@ -25,17 +25,15 @@ class TestPluginAdmonition(TestCase):
         s = '.. note:: Warnning\n\n       print() '
         html = md(s)
         self.assertIn('class="admonition note"', html)
-        self.assertIn('<h1>Warnning</h1>', html)
+        self.assertIn('<p class="admonition-title">Warnning</p>', html)
         self.assertIn('<pre><code>print() \n</code></pre>', html)
 
     def test_note_admonition_no_text(self):
-        md = create_markdown(
-            plugins=[Admonition()]
-        )
+        md = create_markdown(plugins=[Admonition()])
         s = '.. note:: Warnning'
         html = md(s)
         self.assertIn('class="admonition note"', html)
-        self.assertIn('<h1>Warnning</h1>', html)
+        self.assertIn('<p class="admonition-title">Warnning</p>', html)
 
     def test_admonition_options(self):
         md = create_markdown(plugins=[Admonition()])
@@ -43,12 +41,21 @@ class TestPluginAdmonition(TestCase):
         html = md(s)
         self.assertIn('Admonition has no options', html)
 
+    def test_missing_title(self):
+        md = create_markdown(plugins=[Admonition()])
+        s = '.. note::\n\n   message'
+        html = md(s)
+        self.assertIn('<p class="admonition-title">Note</p>', html)
+
+    def test_empty_title(self):
+        md = create_markdown(plugins=[Admonition()])
+        s = '.. note:: ""\n\n   message'
+        html = md(s)
+        self.assertNotIn('<p class="admonition-title">Note</p>', html)
+
     def test_ast_admonition(self):
         data = fixtures.load_json('admonition.json')
-        md = create_markdown(
-            renderer='ast',
-            plugins=[Admonition()]
-        )
+        md = create_markdown(renderer='ast', plugins=[Admonition()])
         # Use JSON to fix the differences between tuple and list
         tokens = json.loads(json.dumps(md(data['text'])))
         self.assertEqual(tokens, data['tokens'])

4fad48f5fc3fb2655eabd54e08f189168428b738
Hsiaoming Yang <me@lepture.com>
2022-07-03 06:06:55 +0900

    Add emoji plugin

---
+++ mistune3/plugins/emoji.py
@@ -0,0 +1,16 @@
+__all__ = ['emoji']
+
+EMOJI = r':[a-z0-9-]+:'
+
+
+def parse_emoji(inline, m, state):
+    text = m.group(0)
+    state.append_token({
+        'type': 'emoji',
+        'raw': text,
+    })
+    return m.end()
+
+
+def emoji(md):
+    md.inline.register_rule('emoji', EMOJI, parse_emoji, before='link')

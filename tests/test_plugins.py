d15d0deefb44467749ab6403c6d6e86258ffc579
Hsiaoming Yang <me@lepture.com>
2022-09-26 20:20:00 +0900

    Improve test cases

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,4 +1,6 @@
 from mistune import create_markdown
+from mistune.plugins.table import table_in_list, table_in_quote
+from mistune.plugins.math import math_in_list, math_in_quote
 from tests import BaseTestCase
 
 
@@ -31,3 +33,33 @@ load_plugin("footnotes")
 load_plugin("abbr")
 load_plugin("math")
 load_plugin("ruby")
+
+
+class TestExtraPlugins(BaseTestCase):
+    def test_table_in_list(self):
+        text = '''- Cell | Cell\n  ---- | ----\n   1  |  2\n'''
+        md1 = create_markdown(escape=False)
+        md2 = create_markdown(escape=False, plugins=['table', table_in_list])
+        self.assertNotIn('<table>', md1(text))
+        self.assertIn('<table>', md2(text))
+
+    def test_table_in_quote(self):
+        text = '''> Cell | Cell\n> ---- | ----\n>  1  |  2\n'''
+        md1 = create_markdown(escape=False)
+        md2 = create_markdown(escape=False, plugins=['table', table_in_quote])
+        self.assertNotIn('<table>', md1(text))
+        self.assertIn('<table>', md2(text))
+
+    def test_math_in_list(self):
+        text = '''- $$\n  foo\n  $$\n'''
+        md1 = create_markdown(escape=False)
+        md2 = create_markdown(escape=False, plugins=['math', math_in_list])
+        self.assertNotIn('class="math"', md1(text))
+        self.assertIn('class="math"', md2(text))
+
+    def test_math_in_quote(self):
+        text = '''> $$\n> foo\n> $$\n'''
+        md1 = create_markdown(escape=False)
+        md2 = create_markdown(escape=False, plugins=['math', math_in_quote])
+        self.assertNotIn('class="math"', md1(text))
+        self.assertIn('class="math"', md2(text))

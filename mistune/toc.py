7b1a28a869ce23a68ce4ef4fb1919f0ec63999f9
Hsiaoming Yang <me@lepture.com>
2022-11-25 20:44:25 +0900

    Fix toc_hook, ref #328

--- mistune/toc.py
+++ mistune/toc.py
@@ -28,9 +28,10 @@ def add_toc_hook(md, min_level=1, max_level=3, heading_id=None):
         headings = []
 
         for tok in state.tokens:
-            level = tok['attrs']['level']
-            if tok['type'] == 'heading' and min_level <= level <= max_level:
-                headings.append(tok)
+            if tok['type'] == 'heading':
+                level = tok['attrs']['level']
+                if min_level <= level <= max_level:
+                    headings.append(tok)
 
         toc_items = []
         for i, tok in enumerate(headings):

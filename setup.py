777db059394600603de9f15f38f30db982cb80ad
Hsiaoming Yang <me@lepture.com>
2023-06-09 22:13:05 +0900

    feat: add py.typed for mypy

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
 from setuptools import setup
 
-# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
-
-setup(
-    name="mistune",
-    package_data={"mistune": ["py.typed", ]}
-)
+setup(name="mistune")

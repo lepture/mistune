bdc01ade171c7c2cd1099e2531ddf8c275a2f64c
Hsiaoming Yang <me@lepture.com>
2026-06-21 22:06:18 +0900

    tests: increase run time on pypy

--- tests/test_security_ref_links.py
+++ tests/test_security_ref_links.py
@@ -1,15 +1,21 @@
+import platform
 import time
 from unittest import TestCase
 
 from mistune import create_markdown
 
+IS_PYPY = platform.python_implementation() == "PyPy"
+
 
 class TestRefLinkSecurity(TestCase):
     def test_many_reference_links_return_quickly(self):
-        text = "".join("[a{}]: u\n".format(i) for i in range(3000))
+        repetitions = 500 if IS_PYPY else 3000
+        deadline = 1.0 if IS_PYPY else 0.5
+
+        text = "".join("[a{}]: u\n".format(i) for i in range(repetitions))
 
         start = time.monotonic()
         create_markdown()(text)
         elapsed = time.monotonic() - start
 
-        self.assertLess(elapsed, 0.5)
+        self.assertLess(elapsed, deadline)

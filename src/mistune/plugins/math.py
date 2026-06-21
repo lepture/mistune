1141eecfcd19f588631e151e213743a78de10daa
Hsiaoming Yang <me@lepture.com>
2026-06-21 19:50:52 +0900

    fix(math): support display and backtick math
    Refs #330, #371, #369.

--- src/mistune/plugins/math.py
+++ src/mistune/plugins/math.py
@@ -9,18 +9,36 @@ if TYPE_CHECKING:
 
 __all__ = ["math", "math_in_quote", "math_in_list"]
 
-BLOCK_MATH_PATTERN = r"^ {0,3}\$\$[ \t]*\n(?P<math_text>[\s\S]+?)\n\$\$[ \t]*$"
-INLINE_MATH_PATTERN = r"\$(?!\$)(?!\s)(?P<math_text>(?:[^$\\\n]|\\.)+?)\$(?!\d)"
+BLOCK_MATH_PATTERN = (
+    r"^ {0,3}\$\$(?P<math_text_single>[^\n]*?)\$\$[ \t]*(?:\n|$)|"
+    r"^ {0,3}\$\$[ \t]*\n(?P<math_text_multi>[\s\S]*?)\n\$\$[ \t]*(?:\n|$)"
+)
+INLINE_MATH_PATTERN = (
+    r"\$\$(?P<display_math_text>(?:[^$\\]|\\.)*?)\$\$|"
+    r"\$(?P<backtick_math_marker>`+)(?P<backtick_math_text>[\s\S]*?)(?P=backtick_math_marker)\$|"
+    r"\$(?!\$)(?!\s)(?P<math_text>(?:[^$\\\n]|\\.)+?)\$(?!\d)"
+)
 
 
 def parse_block_math(block: "BlockParser", m: Match[str], state: "BlockState") -> int:
-    text = m.group("math_text")
+    text = m.group("math_text_single")
+    if text is None:
+        text = m.group("math_text_multi")
+    assert text is not None
     state.append_token({"type": "block_math", "raw": text})
-    return m.end() + 1
+    return m.end()
 
 
 def parse_inline_math(inline: "InlineParser", m: Match[str], state: "InlineState") -> int:
-    text = m.group("math_text")
+    display_text = m.group("display_math_text")
+    if display_text is not None:
+        state.append_token({"type": "block_math", "raw": display_text})
+        return m.end()
+
+    text = m.group("backtick_math_text")
+    if text is None:
+        text = m.group("math_text")
+    assert text is not None
     state.append_token({"type": "inline_math", "raw": text})
     return m.end()
 
@@ -50,7 +68,7 @@ def math(md: "Markdown") -> None:
     :param md: Markdown instance
     """
     md.block.register("block_math", BLOCK_MATH_PATTERN, parse_block_math, before="list")
-    md.inline.register("inline_math", INLINE_MATH_PATTERN, parse_inline_math, before="link")
+    md.inline.register("inline_math", INLINE_MATH_PATTERN, parse_inline_math, before="codespan")
     if md.renderer and md.renderer.NAME == "html":
         md.renderer.register("block_math", render_block_math)
         md.renderer.register("inline_math", render_inline_math)

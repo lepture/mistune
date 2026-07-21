7b0194ce1bd49eca18266722a8704ea619ede0b4
Hsiaoming Yang <me@lepture.com>
2026-07-22 00:56:19 +0900

    test: cover inline performance edge cases

--- tests/test_security_edge_cases.py
+++ tests/test_security_edge_cases.py
@@ -4,6 +4,10 @@ from unittest import TestCase
 from mistune import create_markdown
 
 
+def _best_time(render, size):
+    return min(render(size) for _ in range(3))
+
+
 class TestEdgeCaseSecurity(TestCase):
     def test_blank_list_continuations_are_near_linear(self):
         md = create_markdown()
@@ -29,8 +33,8 @@ class TestEdgeCaseSecurity(TestCase):
             return time.perf_counter() - started
 
         render(1000)
-        small = render(4000)
-        large = render(8000)
+        small = _best_time(render, 4000)
+        large = _best_time(render, 8000)
 
         self.assertLess(large, small * 3.5 + 0.02)
 
@@ -40,3 +44,45 @@ class TestEdgeCaseSecurity(TestCase):
             md("*a" * 6 + "\n"),
             "<p><em>a</em>a<em>a</em>a<em>a</em>a</p>\n",
         )
+
+    def test_mixed_emphasis_delimiters_are_near_linear(self):
+        md = create_markdown()
+
+        def render(size):
+            started = time.perf_counter()
+            md("*a_b" * size)
+            return time.perf_counter() - started
+
+        render(500)
+        small = _best_time(render, 2000)
+        large = _best_time(render, 4000)
+
+        self.assertLess(large, small * 3 + 0.05)
+
+    def test_malformed_images_are_near_linear(self):
+        md = create_markdown()
+
+        def render(size):
+            started = time.perf_counter()
+            md("![" * size + "x" + "]" * size)
+            return time.perf_counter() - started
+
+        render(500)
+        small = _best_time(render, 2000)
+        large = _best_time(render, 4000)
+
+        self.assertLess(large, small * 3 + 0.05)
+
+    def test_unclosed_link_destinations_are_near_linear(self):
+        md = create_markdown()
+
+        def render(size):
+            started = time.perf_counter()
+            md("[](" * size)
+            return time.perf_counter() - started
+
+        render(500)
+        small = _best_time(render, 2000)
+        large = _best_time(render, 4000)
+
+        self.assertLess(large, small * 3 + 0.05)
